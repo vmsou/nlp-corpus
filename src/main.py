147c2b0cfd532c21029b37ee8cd844264187020b
vmsou <viniciusmarcs@icloud.com>
2022-10-09 22:27:03 -0300

    add: reports from text

--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
+import os
+
 import requests
 
 import spacy
 from bs4 import BeautifulSoup
+from spacy.tokens.doc import Doc
 
 NLP: spacy.Language = spacy.load("en_core_web_sm")
 
 
-def generate_reports(text: str) -> list[str]: ...
+def reports_from_text(text: str) -> list[str]:
+    """ Extracts sentences from text string. """
+    document: Doc = NLP(text)
+    return [sent.text for sent in document.sents]
 
 
 def reports_from_site(url: str) -> list[str]:
+    """ Extracts sentences from website (PDF or HTML).  """
     reports: list[str] = []
     response: requests.Response = requests.get(url)
     if response.status_code != 200:
         return reports
     soup: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
-    # TODO: Scrape site/pdf for sentences
+    # TODO: Scrape html/pdf for sentences
     return reports
 
 
 def scrape_scholar(query: str, limit: int = 5, lang: str = "en") -> list[dict[str, str]]:
+    """ Generates dict with information from Google Scholar articles. """
     articles: list[dict[str, str]] = []
     params: dict[str, str] = dict(q=query, hl=lang)
     response: requests.Response = requests.get("https://scholar.google.com.br/scholar", params=params)
@@ -38,8 +46,12 @@ def scrape_scholar(query: str, limit: int = 5, lang: str = "en") -> list[dict[st
 
 def main() -> None:
     subject: str = "Natural Language Processing"
-    articles: list[dict[str, str]] = scrape_scholar(subject, 5, "en")
-    print(*articles, sep='\n')
+    # articles: list[dict[str, str]] = scrape_scholar(subject, 5, "en")
+    # print(*articles, sep='\n')
+    with open("../resources/article.txt", 'r', encoding="UTF-8") as f:
+        reports: list[str] = reports_from_text(f.read())
+        for report in reports:
+            print(">", report)
 
 
 if __name__ == "__main__":
