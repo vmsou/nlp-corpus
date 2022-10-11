bb17479a85dc52b9e2f7e6673164e09bf8807eac
vmsou <viniciusmarcs@icloud.com>
2022-10-11 14:20:15 -0300

    add: typings and doc

--- src/main.py
+++ src/main.py
@@ -1,57 +1,78 @@
-import os
+from typing import TypedDict
 
 import requests
+import logging
 
 import spacy
-from bs4 import BeautifulSoup
+import bs4
 from spacy.tokens.doc import Doc
 
+# Config
+logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
 NLP: spacy.Language = spacy.load("en_core_web_sm")
 
 
+# Type Alias
+class ArticleDict(TypedDict):
+    title: str
+    link: str
+    doc_link: str
+
+
+# Functions
 def reports_from_text(text: str) -> list[str]:
     """ Extracts sentences from text string. """
+    logging.info(f"Generating reports from text: {text[:20]}...")
     document: Doc = NLP(text)
-    return [sent.text for sent in document.sents]
+    reports: list[str] = [sent.text for sent in document.sents]
+    logging.info(f"Done. Reports generated from text: {text[:20]}...")
+    return reports
 
 
 def reports_from_site(url: str) -> list[str]:
     """ Extracts sentences from website (PDF or HTML).  """
+    logging.info(f"Generating reports from site: {url}...")
     reports: list[str] = []
     response: requests.Response = requests.get(url)
     if response.status_code != 200:
         return reports
-    soup: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
+    soup: bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, "html.parser")
     # TODO: Scrape html/pdf for sentences
+    logging.info(f"Done. Reports generated from site: {url}...")
     return reports
 
 
-def scrape_scholar(query: str, limit: int = 5, lang: str = "en") -> list[dict[str, str]]:
+def scrape_scholar(query: str, limit: int = 5, lang: str = "en") -> list[ArticleDict]:
     """ Generates dict with information from Google Scholar articles. """
+    logging.info(f"Scraping {query} from Google Scholar...")
+
     articles: list[dict[str, str]] = []
     params: dict[str, str] = dict(q=query, hl=lang)
     response: requests.Response = requests.get("https://scholar.google.com.br/scholar", params=params)
     if response.status_code != 200: return articles
 
-    soup: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
+    soup: bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, "html.parser")
 
     for article_div in soup.select(".gs_r.gs_or.gs_scl")[:limit]:
         title: str = article_div.select_one(".gs_rt a").text
         link: str = article_div.select_one(".gs_rt a")["href"]
         doc_link: str = article_div.select_one(".gs_or_ggsm a")["href"]
-        article: dict[str, str] = dict(title=title, link=link, doc_link=doc_link)
+        article: ArticleDict = dict(title=title, link=link, doc_link=doc_link)
         articles.append(article)
+    logging.info(f"Done. Articles scraped from query: {query}...")
     return articles
 
 
 def main() -> None:
+    logging.debug("Program Start")
     subject: str = "Natural Language Processing"
-    # articles: list[dict[str, str]] = scrape_scholar(subject, 5, "en")
+    # articles: list[dict[str, str]] =
     # print(*articles, sep='\n')
-    with open("../resources/article.txt", 'r', encoding="UTF-8") as f:
-        reports: list[str] = reports_from_text(f.read())
-        for report in reports:
-            print(">", report)
+    for article in scrape_scholar(subject, 5, "en"):
+        url: str = article["doc_link"]
+        reports: list[str] = reports_from_site(url)
+
+    logging.debug("Program End")
 
 
 if __name__ == "__main__":
